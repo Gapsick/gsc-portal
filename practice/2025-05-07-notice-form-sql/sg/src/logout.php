a7adcf343130960d3000d0261090fe9ed07aeb61
Theunkilabledemonking <abcqkdnxm@g.yju.ac.kr>
2025-05-06 19:27:28 +0900

    Apache 가상 호스트 설정 및 공지사항 서비스 배포 디렉토리 연결 완료

--- practice/2025-05-07-notice-form-sql/sg/src/logout.php
+++ practice/2025-05-07-notice-form-sql/sg/src/logout.php
@@ -1,7 +1,7 @@
 <?php
 session_start(); // 세션 시작 (사용 중이어야 파괴 가능)
 session_unset(); // 모든 세션 변수 제거
-session_destroy(); // 세션 자체ㅡㄹ 파괴
+session_destroy(); // 세션 자체를 파괴
 
 // 로그인 페이지로 이동
 header("Location: login.php");
